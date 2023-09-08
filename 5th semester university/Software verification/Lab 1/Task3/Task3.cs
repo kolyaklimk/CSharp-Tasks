7306b65ab0dde92a0255050ff270a7e64239c01f
Kolya <kolyaklimk@gmail.com>
2023-09-08 23:54:29 +0300

    Add task3

---
+++ 5th semester university/Software verification/Lab 1/Task3/Task3.cs
@@ -0,0 +1,80 @@
+﻿using Lab_1.Other;
+using System;
+
+namespace Lab_1.Task3
+{
+    public static class Task3
+    {
+
+        public static void StartTask()
+        {
+            MyConsole.printTasks(3);
+            ReadConsole();
+        }
+
+        public static double CheckData(string text)
+        {
+            double a;
+            if (!double.TryParse(text, out a))
+            {
+                return -1;
+            }
+            if (a <= 0)
+            {
+                return -1;
+            }
+
+            return a;
+        }
+
+        public static string GetS(double a, double b)
+        {
+            try
+            {
+                var s = a * b;
+                return s.ToString();
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+
+        public static void ReadConsole()
+        {
+            double a;
+            while (true)
+            {
+                Console.WriteLine("Print a:");
+                var text = Console.ReadLine();
+                a = CheckData(text);
+                if (a != -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again");
+                }
+            }
+
+            double b;
+            while (true)
+            {
+                Console.WriteLine("Print b:");
+                var text = Console.ReadLine();
+                b = CheckData(text);
+                if (b != -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error, try again");
+                }
+            }
+
+            Console.WriteLine($"S = {GetS(a, b)}");
+        }
+    }
+}
