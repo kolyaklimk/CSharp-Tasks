eef2c35f198d918d6421a0ec62efcc82f921fc3d
Nikolay Klimkovich <kolyaklimk@gmail.com>
2023-09-08 12:15:08 +0300

    Add task1

---
+++ 5th semester university/Software verification/Lab 1/Lab 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Task1.StartTask();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5th semester university/Software verification/Lab 1/Lab 1/Task1/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_1.Task1
+{
+    public static class Task1
+    {
+        public static void StartTask()
+        {
+            Console.WriteLine(GenerateString());
+        }
+
+        public static string GenerateString()
+        {
+            Random rand = new Random();
+            return $"Hello, world!\nAndhiagain!\n{new string('!', rand.Next(5, 50))}";
+        }
+    }
+}
