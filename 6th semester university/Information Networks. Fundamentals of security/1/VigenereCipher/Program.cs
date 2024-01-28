2ac34cf8a21dd4fd55608177a57d97839d0a96f7
Nikolay Klimkovich <kolyaklimk@gmail.com>
2024-01-28 19:41:38 +0300

    Add Vigenere

--- 6th semester university/Information Networks. Fundamentals of security/1/VigenereCipher/Program.cs
+++ 6th semester university/Information Networks. Fundamentals of security/1/VigenereCipher/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+class VigenereCipher
+{
+    private static string inputFilePath =
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+    private static string encryptFilePath =
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "encrypt.txt");
+    private static string decryptFilePath =
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "decrypt.txt");
+
+    static void Main()
+    {
+        Console.Write("Vigenere cipher:\nKey: ");
+        string key = Console.ReadLine().ToUpper();
+
+        EncryptFile(key);
+        DecryptFile(key);
+
+        Console.ReadKey();
+    }
+
+    static void EncryptFile(string key)
+    {
+        string plainText = File.ReadAllText(inputFilePath);
+        string encryptedText = Vigenere(plainText, key);
+        File.WriteAllText(encryptFilePath, encryptedText);
+        Console.WriteLine($"\nEncrypted text in encrypt.txt:\n{encryptedText}");
+    }
+
+    static void DecryptFile(string key)
+    {
+        string encryptedText = File.ReadAllText(encryptFilePath);
+        string decryptedText = Vigenere(encryptedText, key, false);
+        File.WriteAllText(decryptFilePath, decryptedText);
+        Console.WriteLine($"\nDecrypted text in decrypt.txt:\n{decryptedText}");
+    }
+
+    static string Vigenere(string text, string key, bool isEncrypt = true)
+    {
+        StringBuilder result = new StringBuilder();
+        int keyIndex = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsLetter(text[i]))
+            {
+                char offset = char.IsUpper(text[i]) ? 'A' : 'a';
+                int keyValue = key[keyIndex] - 'A';
+                char encryptedChar = (char)((text[i] - offset + (isEncrypt ? keyValue : 26 - keyValue)) % 26 + offset);
+                result.Append(encryptedChar);
+
+                keyIndex = (keyIndex + 1) % key.Length;
+            }
+            else
+            {
+                result.Append(text[i]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
