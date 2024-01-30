ca14cb338f9105d8a17bb6d278664db81feeb47a
Nikolay Klimkovich <kolyaklimk@gmail.com>
2024-01-30 19:47:05 +0300

    Create MAUI project with base interface

---
+++ 6th semester university/Information Networks. Fundamentals of security/2/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace _2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string id = Text.Text;
+        }
+    }
+}
