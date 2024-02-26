ba80934ba03b25f652702fdb4b5c4903fd470fa2
Nikolay Klimkovich <kolyaklimk@gmail.com>
2024-02-26 11:01:47 +0300

    Update Client.cs

--- 6th semester university/Information Networks. Fundamentals of security/2/KerberosApplication/Client.cs
+++ 6th semester university/Information Networks. Fundamentals of security/2/KerberosApplication/Client.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace _2.KerberosApplication
 {
-    internal class Client
+
+    class Client
     {
+        public string UserName { get; private set; }
+        private string Password { get; set; }
+
+        public string PasswordHash
+        {
+            get => GetPasswordHash();
+        }
+
+        public Client(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public string GetPasswordHash()
+        {
+            var tmpSource = Encoding.ASCII.GetBytes(Password);
+            var tmpHash = MD5.HashData(tmpSource);
+            var value = new StringBuilder(tmpHash.Length);
+            foreach (byte b in tmpHash)
+            {
+                value.Append(b.ToString("X2"));
+            }
+
+            return value.ToString();
+        }
     }
 }
